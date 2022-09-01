dff240cbc74cac339085a56072c851ab07bb0f43
JianboYang <jyan0173@student.monash.edu>
2022-09-01 16:31:25 +0800

    Add project files.

---
+++ FIT5032_WEEK4_DataTable/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FIT5032_WEEK4_DataTable.Startup))]
+namespace FIT5032_WEEK4_DataTable
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
